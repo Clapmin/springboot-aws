e4ce05ed83772e6028c609ac0c948d8d6556b72f
clapmin <minclap92@gmail.com>
2021-01-04 20:44:11 +0900

    - add google login - windows 환경변수 %username% 영향으로, 이름은 memberName으로 사용

---
+++ src/main/java/com/sment/board/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.sment.board.springboot.config.auth;
+
+import com.sment.board.springboot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity//TODO ??
+public class SecurityConfig extends WebSecurityConfigurerAdapter {//TODO ??
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                    .anyRequest().authenticated()
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login()
+                        .userInfoEndpoint()
+                            .userService(customOAuth2UserService);
+    }
+}
